8243b4965684fe81a7823b8999d817775df11f40
mijan <jnajdic3919rn@raf.rs>
2021-11-19 00:55:58 +0100

    Procedura4 completed! YEET!

--- ProcedureImpl/src/Procedure/Procedura4.java
+++ ProcedureImpl/src/Procedure/Procedura4.java
@@ -1,8 +1,46 @@
 package Procedure;
 
+import java.sql.*;
+
 public class Procedura4 {
+    public static void main(String[] args) throws SQLException {
 
-    public static void main(String[] args) {
+        Connection myConn = null;
+        CallableStatement myCall = null;
+        ResultSet myRS = null;
 
+        try {
+
+            myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/project01", "student", "student");
+
+            myCall = myConn.prepareCall("{call Procedura4(?)}");
+
+            myCall.setString(1, "A Bike Store");
+            myCall.execute();
+
+            myRS = myCall.getResultSet();
+
+            while(myRS.next()){
+                /// Name, NumberOfOrders, AverageNumberOfArticles, TotalSales
+                System.out.println("Store " + myRS.getString("Name") + " had " + myRS.getInt("NumberOfOrders") + " orders with average number of saled items per order " +
+                        myRS.getFloat("AverageNumberOfArticles") + " and total sale " + myRS.getFloat("TotalSales"));
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+
+            if (myConn != null) {
+                myConn.close();
+            }
+
+            if (myCall != null) {
+                myCall.close();
+            }
+
+            if (myRS != null) {
+                myRS.close();
+            }
+        }
     }
 }
