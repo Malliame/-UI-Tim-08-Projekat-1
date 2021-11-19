118ad132544dfe127a6268ce63bdd05600b3cdc0
mijan <jnajdic3919rn@raf.rs>
2021-11-19 01:44:40 +0100

    Well well...

--- ProcedureImpl/src/Procedure/Procedura4.java
+++ ProcedureImpl/src/Procedure/Procedura4.java
@@ -1,6 +1,7 @@
 package Procedure;
 
 import java.sql.*;
+import java.util.Scanner;
 
 public class Procedura4 {
     public static void main(String[] args) throws SQLException {
@@ -8,22 +9,32 @@ public class Procedura4 {
         Connection myConn = null;
         CallableStatement myCall = null;
         ResultSet myRS = null;
+        Scanner sc = new Scanner(System.in);
+        String input = "";
 
         try {
 
-            myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/adventureworks", "student", "student");
+            myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/project01", "student", "student");
 
-            myCall = myConn.prepareCall("{call Procedura4(?)}");
+            while(true) {
+                myCall = myConn.prepareCall("{call Procedura4(?)}");
 
-            myCall.setString(1, "A Bike Store");
-            myCall.execute();
+                System.out.println("Enter store name or exit to quit: ");
+                input = sc.nextLine();
 
-            myRS = myCall.getResultSet();
+                if(input.equals("exit"))
+                    break;
 
-            while(myRS.next()){
-                /// Name, NumberOfOrders, AverageNumberOfArticles, TotalSales
-                System.out.println("Store " + myRS.getString("Name") + " had " + myRS.getInt("NumberOfOrders") + " orders with average number of saled items per order " +
-                        myRS.getFloat("AverageNumberOfArticles") + " and total sale " + myRS.getFloat("TotalSales"));
+                myCall.setString(1, input);
+                myCall.execute();
+
+                myRS = myCall.getResultSet();
+
+                while (myRS.next()) {
+                    /// Name, NumberOfOrders, AverageNumberOfArticles, TotalSales
+                    System.out.println("Store " + myRS.getString("Name") + " had " + myRS.getInt("NumberOfOrders") + " orders with average number of saled items per order " +
+                            myRS.getFloat("AverageNumberOfArticles") + " and total sale " + myRS.getFloat("TotalSales"));
+                }
             }
 
         } catch (Exception e) {
